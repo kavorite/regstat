45da4d794d43305854eecd4228d07b2a65f81f7a
æriess <iseurie@gmail.com>
2020-06-12 16:50:58 -0400

    syntax fixes

--- main.py
+++ main.py
@@ -4,11 +4,11 @@ import csv
 import time
 import asyncio
 import itertools as it
-from dataclasses import dataclass
+import dataclasses
 from datetime import date
 
 
-@dataclass
+@dataclasses.dataclass
 class Contact(object):
     surname: str
     house_nr: str
@@ -16,40 +16,39 @@ class Contact(object):
     zipcode: str
     dob: date
 
-    cdl_dat_row_indices_by_attr = {
+    cdl_dat_schema = {
         'surname': 1, 'house_nr': 5, 'street': 6,
         'zipcode': 13, 'dob': 27,
     }
 
-    boe_row_indices_by_attr = {
+    boe_schema = {
         'surname': 6, 'house_nr': 9, 'street': 10,
         'zipcode': 14, 'dob': 15
     }
 
     @classmethod
-    def from_row(cls, row, row_indices_by_attr=cls.cdl_dat_row_indices_by_attr):
-        attr_names = ('surname', 'house_nr', 'street', 'zipcode', 'dob')
-        attr_indices = {attr: argpos for argpos, attr in enumerate(attr_names)}
-        attr_values = tuple(None for i in range(len(fields))
-        for attr, index in row_indices_by_attr:
+    def from_row(cls, row, schema):
+        attr_fields = dataclasses.fields(cls)
+        attr_indices = {attr: argpos for argpos, attr
+                        in enumerate(f.name for f in attr_fields)}
+        attr_values = [None for i in range(len(attr_indices))]
+        for attr, index in schema.items():
             if attr not in attr_indices:
                 raise ValueError('invalid field')
-            attr_values[attr_indices[attr]] = row[index]
+            cell = row[index]
+            if attr == 'dob':
+                month, day, year = map(int, cell.split('/', 3))
+                attr_values[attr_indices[attr]] = date(year, month, day)
+                continue
+            attr_values[attr_indices[attr]] = cell
         if None in attr_values:
-            missing = (attr_names[i] for i, v in enumerate(attr_values)
+            missing = (attr_fields[i].name for i, v in enumerate(attr_values)
                        if v is None)
             missing_enum = ', '.join(f"'{field}'" for field in missing)
-            msg = f'row_indices_by_attr missing entries for fields {missing_enum}'
+            msg = (r'row_indices_by_attr missing entries '
+                   f'for fields {missing_enum}')
             raise ValueError(msg)
-        return cls(*args)
-
-    @classmethod
-    def from_cdl_dat_row(cls, row):
-        return cls.from_row(cls, row, cls.cdl_dat_row_indices_by_attr)
-
-    @classmethod
-    def from_boe_row(cls, row):
-        return cls.from_row(cls, row, cls.boe_row_indices_by_attr)
+        return cls(*attr_values)
 
     async def status(self, session):
         endpoint = 'https://www.monroecounty.gov/etc/voter/index.php'
@@ -57,7 +56,7 @@ class Contact(object):
                 'v[lname]': self.surname,
                 'v[no]': self.house_nr,
                 'v[sname]': self.street,
-                'v[zip]': self.zip,
+                'v[zip]': self.zipcode,
                 'v[dobm]': self.dob.month,
                 'v[dobd]': self.dob.day,
                 'v[doby]': self.dob.year,
@@ -68,10 +67,8 @@ class Contact(object):
         regstat = summary['class'][0]
         children = list(markup.select('div#voter > div')[1].descendants)
         party = children[3].strip()
-        try:
-            ballot_stat = children[6].strip()
-        except IndexError:
-            ballot_stat = ''
+        ballot_stat = ('' if 6 not in range(len(children))
+                       else children[6].strip())
         return (regstat, party, ballot_stat)
 
 
@@ -85,12 +82,16 @@ async def main(records, steno):
 
     parser.add_argument('--schema', default='cdl', type=boe_or_cdl)
     args = parser.parse_args()
-    fieldmap = (Contact.cdl_dat_row_indices_by_attr
-                if args.schema == 'boe'
-                else Contact.boe_row_indices_by_attr)
+    schema = (Contact.boe_schema
+              if args.schema == 'boe'
+              else Contact.cdl_dat_schema)
 
     async def write_status_annotated(record, ostrm, session):
-        stat = await Contact(record).status(session)
+        try:
+            contact = Contact.from_row(record, schema)
+        except ValueError:
+            return
+        stat = await contact.status(session)
         record += stat
         ostrm.writerow(record)
 
@@ -109,7 +110,9 @@ async def main(records, steno):
                     await done.pop()
                 finally:
                     in_flight -= done
-            in_flight |= {asyncio.ensure_future(write_status_annotated(record, steno, s))}
+            in_flight |= {
+                asyncio.ensure_future(write_status_annotated(record, steno, s))
+            }
         if len(in_flight) > 0:
             await asyncio.wait(in_flight)
 
